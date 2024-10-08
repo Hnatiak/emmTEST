11c9937ff6650098f19cca4ae39aa6b63de4235e
Hnatiak <58102343+Hnatiak@users.noreply.github.com>
2024-10-08 22:10:27 +0200

    Version 0.3

--- script.js
+++ script.js
@@ -1,1 +1,106 @@
-function _0x29ae(){const _0x358c8e=['2MZxWeo','33uYINEO','font-size:\x2020px;\x20color:\x20black;','innerHTML','clear','href','_commandLineAPI','append','%cЗупиніться!','json','Всі\x20твої\x20дані\x20було\x20успішно\x20відправлено\x20анонімусу\x20а\x20також\x20тобі\x20подарунок,\x20я\x20скачав\x20тобі\x20твої\x20дані\x20в\x20текстовому\x20форматі,\x20дякую\x20за\x20участь!','proxy','lat','textContent','createObjectURL','http://ip-api.com/json/','appendChild','Регіон:\x20','9JHuGHv','body','1189242ufMVTB','8380wmhZpq','createElement','log','defineProperty','download','294132OlNweO','Країна:\x20','country','?fields=status,message,country,countryCode,region,regionName,city,zip,lat,lon,isp,org,as,query','text/plain','А\x20я\x20бачу\x20ти\x20використовуєш\x20VPN','user_info.txt','countryCode','\x0a\x20\x20\x20\x20','load','ВАШЕ\x20IP:\x20','%cЦя\x20функція\x20браузера\x20призначена\x20для\x20розробників.\x20Якщо\x20хтось\x20сказав\x20вам\x20щось\x20скопіювати\x20й\x20сюди\x20вставити,\x20щоб\x20увімкнути\x20функцію\x20або\x20\x22зламати\x22\x20чийсь\x20обліковий\x20запис,\x20це\x20шахраї.\x20Виконавши\x20ці\x20дії,\x20ви\x20надасте\x20їм\x20доступ\x20до\x20свого\x20облікового\x20запису.','1675429JTVHEQ','color:\x20red;\x20font-size:\x2050px;\x20font-weight:\x20bold;','catch','click','regionName','978wNCLJr','Місто:\x20','content','\x0a\x20\x20\x20\x20\x20\x20\x20\x20Місто:\x20','lon','city','then','\x0a\x20\x20\x20\x20\x20\x20\x20\x20Місце\x20знаходження:\x20','290632dlsJHp','758850pezDDH','Твоє\x20місце\x20знаходження:\x20','1623176lzWVrX','\x0a\x20\x20\x20\x20\x20\x20\x20\x20Код\x20країни:\x20','Доступ\x20до\x20консолі\x20заборонено!','getElementById'];_0x29ae=function(){return _0x358c8e;};return _0x29ae();}function _0x1403(_0x2cbf47,_0x43d8c0){const _0x29ae88=_0x29ae();return _0x1403=function(_0x1403c7,_0xf02616){_0x1403c7=_0x1403c7-0x1c7;let _0x75e8f6=_0x29ae88[_0x1403c7];return _0x75e8f6;},_0x1403(_0x2cbf47,_0x43d8c0);}const _0x5210c3=_0x1403;(function(_0x120087,_0xfd9a43){const _0x397f1e=_0x1403,_0x31af13=_0x120087();while(!![]){try{const _0x5b67f0=-parseInt(_0x397f1e(0x1cc))/0x1*(-parseInt(_0x397f1e(0x1e6))/0x2)+-parseInt(_0x397f1e(0x1de))/0x3*(-parseInt(_0x397f1e(0x1ff))/0x4)+-parseInt(_0x397f1e(0x1e1))/0x5*(parseInt(_0x397f1e(0x1f7))/0x6)+-parseInt(_0x397f1e(0x1f2))/0x7+-parseInt(_0x397f1e(0x1c8))/0x8+parseInt(_0x397f1e(0x1e0))/0x9+-parseInt(_0x397f1e(0x200))/0xa*(-parseInt(_0x397f1e(0x1cd))/0xb);if(_0x5b67f0===_0xfd9a43)break;else _0x31af13['push'](_0x31af13['shift']());}catch(_0x5eb7f0){_0x31af13['push'](_0x31af13['shift']());}}}(_0x29ae,0x26333),(function(){const _0x3eb223=_0x1403;console[_0x3eb223(0x1e3)]('%cЗупиніться!',_0x3eb223(0x1f3)),console['log']('%cЦя\x20функція\x20браузера\x20призначена\x20для\x20розробників.\x20Якщо\x20хтось\x20сказав\x20вам\x20щось\x20скопіювати\x20й\x20сюди\x20вставити,\x20щоб\x20увімкнути\x20функцію\x20або\x20\x22зламати\x22\x20чийсь\x20обліковий\x20запис,\x20це\x20шахраї.\x20Виконавши\x20ці\x20дії,\x20ви\x20надасте\x20їм\x20доступ\x20до\x20свого\x20облікового\x20запису.',_0x3eb223(0x1ce)),Object[_0x3eb223(0x1e4)](console,_0x3eb223(0x1d2),{'get':function(){const _0x1428d1=_0x3eb223;throw _0x1428d1(0x1ca);}}),setInterval(function(){const _0x408be0=_0x3eb223;console[_0x408be0(0x1d0)](),console[_0x408be0(0x1e3)](_0x408be0(0x1d4),_0x408be0(0x1f3)),console[_0x408be0(0x1e3)](_0x408be0(0x1f1),_0x408be0(0x1ce));},0x3e8);}()),addEventListener(_0x5210c3(0x1ef),_0x5a8a7e=>{fetchData();}));async function fetchData(){const _0xf4120b=_0x5210c3,_0x46dc50=await fetch('https://api.ipify.org?format=json')[_0xf4120b(0x1fd)](_0x4c33aa=>_0x4c33aa['json']())['then'](_0x580a58=>_0x580a58)[_0xf4120b(0x1f4)](_0x4435d4=>console[_0xf4120b(0x1e3)]('Could\x20not\x20find\x20IP\x20address',_0x4435d4)),_0xa31b52=await fetch(_0xf4120b(0x1db)+_0x46dc50['ip']+_0xf4120b(0x1e9))[_0xf4120b(0x1fd)](_0x3226d7=>_0x3226d7[_0xf4120b(0x1d5)]())[_0xf4120b(0x1fd)](_0x5ef98e=>_0x5ef98e)[_0xf4120b(0x1f4)](_0x323787=>console[_0xf4120b(0x1e3)]('Could\x20not\x20find\x20user\x20info',_0x323787));console[_0xf4120b(0x1e3)](_0xa31b52);const _0x564640=document[_0xf4120b(0x1cb)](_0xf4120b(0x1f9)),_0x578e53=document[_0xf4120b(0x1e2)]('h1');_0x578e53[_0xf4120b(0x1d9)]=_0xf4120b(0x1c7)+_0xa31b52[_0xf4120b(0x1d8)]+'\x20'+_0xa31b52[_0xf4120b(0x1fb)];const _0x24be8a=document[_0xf4120b(0x1e2)]('h4');_0x24be8a[_0xf4120b(0x1d9)]=_0xf4120b(0x1e7)+_0xa31b52['country'];const _0x1c9c9a=document['createElement']('h4');_0x1c9c9a[_0xf4120b(0x1d9)]='Код\x20країни:\x20'+_0xa31b52['countryCode'];const _0x52d6b6=document['createElement']('h4');_0x1c9c9a['textContent']=_0xf4120b(0x1f0)+_0x46dc50['ip'];const _0x9d1a7d=document['createElement']('h4');_0x9d1a7d[_0xf4120b(0x1d9)]=_0xf4120b(0x1f8)+_0xa31b52[_0xf4120b(0x1fc)];const _0x433a05=document[_0xf4120b(0x1e2)]('h4');_0x433a05['textContent']=_0xf4120b(0x1dd)+_0xa31b52['regionName'];const _0x5db85a=document['createElement']('h2');_0x5db85a[_0xf4120b(0x1d9)]=_0xf4120b(0x1d6),_0x564640[_0xf4120b(0x1d3)](_0x52d6b6,_0x578e53,_0x24be8a,_0x1c9c9a,_0x9d1a7d,_0x433a05,_0x5db85a);const _0x29e70a=_0xf4120b(0x1fe)+_0xa31b52[_0xf4120b(0x1d8)]+',\x20'+_0xa31b52['lon']+'\x0a\x20\x20\x20\x20\x20\x20\x20\x20Країна:\x20'+_0xa31b52[_0xf4120b(0x1e8)]+_0xf4120b(0x1c9)+_0xa31b52[_0xf4120b(0x1ed)]+_0xf4120b(0x1fa)+_0xa31b52[_0xf4120b(0x1fc)]+'\x0a\x20\x20\x20\x20\x20\x20\x20\x20Регіон:\x20'+_0xa31b52[_0xf4120b(0x1f6)]+_0xf4120b(0x1ee),_0x39711f=new Blob([_0x29e70a],{'type':_0xf4120b(0x1ea)}),_0x5ae81a=window['URL'][_0xf4120b(0x1da)](_0x39711f),_0x3e727f=document[_0xf4120b(0x1e2)]('a');_0x3e727f[_0xf4120b(0x1d1)]=_0x5ae81a,_0x3e727f[_0xf4120b(0x1e5)]=_0xf4120b(0x1ec),document[_0xf4120b(0x1df)][_0xf4120b(0x1dc)](_0x3e727f),_0x3e727f[_0xf4120b(0x1f5)](),document[_0xf4120b(0x1df)]['removeChild'](_0x3e727f);if(_0xa31b52[_0xf4120b(0x1ed)]==='UA')_0x564640[_0xf4120b(0x1cf)]='Україна';else{if(_0xa31b52[_0xf4120b(0x1d7)])_0x564640[_0xf4120b(0x1cf)]=_0xf4120b(0x1eb);}}
+(function() {
+    // Виведення великого попередження у консоль
+    console.log('%cЗупиніться!', 'color: red; font-size: 50px; font-weight: bold;');
+    console.log('%cЦя функція браузера призначена для розробників. Якщо хтось сказав вам щось скопіювати й сюди вставити, щоб увімкнути функцію або "зламати" чийсь обліковий запис, це шахраї. Виконавши ці дії, ви надасте їм доступ до свого облікового запису.', 'font-size: 20px; color: black;');
+  
+    // Заборона введення або доступ до консолі
+    Object.defineProperty(console, '_commandLineAPI', {
+      get: function() {
+        throw 'Доступ до консолі заборонено!';
+      }
+    });
+  
+    // Очищення консольних повідомлень кожну секунду
+    setInterval(function() {
+      console.clear();
+      console.log('%cЗупиніться!', 'color: red; font-size: 50px; font-weight: bold;');
+      console.log('%cЦя функція браузера призначена для розробників. Якщо хтось сказав вам щось скопіювати й сюди вставити, щоб увімкнути функцію або "зламати" чийсь обліковий запис, це шахраї. Виконавши ці дії, ви надасте їм доступ до свого облікового запису.', 'font-size: 20px; color: black;');
+    }, 1000);
+  })();
+
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'F12') {
+        e.preventDefault();
+    }
+});
+
+// Блокування правого кліку
+document.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+});
+
+
+addEventListener('load', (event) => {
+    fetchData()
+})
+
+async function fetchData() {
+    const userIP = await fetch('https://api.ipify.org?format=json')
+        .then(response => response.json())
+        .then(result => result)
+        .catch(error => console.log('Could not find IP address', error))
+
+    const userInfo = await fetch(`http://ip-api.com/json/${userIP.ip}?fields=status,message,country,countryCode,region,regionName,city,zip,lat,lon,isp,org,as,query`)
+        .then(response => response.json())
+        .then(result => result)
+        .catch(error => console.log('Could not find user info', error))
+    console.log(userInfo)
+
+    const contentElement = document.getElementById('content')
+
+    // Створення елементів
+    const h1 = document.createElement('h1')
+    h1.textContent = `Твоє місце знаходження: ${userInfo.lat} ${userInfo.lon}`
+
+    const h3 = document.createElement('h4')
+    h3.textContent = `Країна: ${userInfo.country}`
+
+    const h4 = document.createElement('h4')
+    h4.textContent = `Код країни: ${userInfo.countryCode}`
+
+    const ip = document.createElement('h4')
+    h4.textContent = `ВАШЕ IP: ${userIP.ip}`
+
+    const city = document.createElement('h4')
+    city.textContent = `Місто: ${userInfo.city}`
+
+    const regionName = document.createElement('h4')
+    regionName.textContent = `Регіон: ${userInfo.regionName}`
+
+    const h2 = document.createElement('h2')
+    h2.textContent = `Всі твої дані було успішно відправлено анонімусу а також тобі подарунок, я скачав тобі твої дані в текстовому форматі, дякую за участь!`
+
+    contentElement.append(ip, h1, h3, h4, city, regionName, h2)
+
+    // Дані для збереження в файл
+    const data = `
+        Місце знаходження: ${userInfo.lat}, ${userInfo.lon}
+        Країна: ${userInfo.country}
+        Код країни: ${userInfo.countryCode}
+        Місто: ${userInfo.city}
+        Регіон: ${userInfo.regionName}
+    `;
+
+    // Створюємо новий Blob з текстовими даними
+    const blob = new Blob([data], { type: 'text/plain' });
+
+    // Створюємо URL для Blob
+    const url = window.URL.createObjectURL(blob);
+
+    // Створюємо посилання для завантаження файлу
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'user_info.txt';  // Назва файлу
+
+    // Додаємо посилання до тіла документа та автоматично клікаємо для завантаження
+    document.body.appendChild(a);
+    a.click();
+
+    // Видаляємо посилання після завантаження
+    document.body.removeChild(a);
+
+    // Додатковий вивід для виявлення використання проксі/VPN
+    if (userInfo.countryCode === 'UA') contentElement.innerHTML = 'Україна';
+    else if (userInfo.proxy) contentElement.innerHTML = 'А я бачу ти використовуєш VPN';
+}
